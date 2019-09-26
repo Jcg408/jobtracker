0fbef8abb824f4540d9e97d214f728206542deca
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-09-26 18:56:33 -0400

    rearrange directory in order to use concurrently

---
+++ client/src/components/JobCreate.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+
+class JobCreate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            company: "",
+            contact: "",
+            action: "",
+            date: new Date(),
+            note: "",
+
+        }
+    }
+
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleDate = (date) => {
+        this.setState({
+            date: date
+        })
+    }
+    
+    handleSubmit = (event) => {
+    
+        event.preventDefault();
+        const job = {
+            title: this.state.title,
+            company: this.state.company,
+            contact: this.state.contact,
+            action: this.state.action,
+            date: this.state.date,
+            note: this.state.note
+        }
+        console.log(job);
+        axios.post('http://localhost:5000/jobs/add', job)
+        .then(res=> console.log(res.data));
+
+        window.location = '/list';
+        
+    }
+
+    render() {
+        return (
+            <div className='container' id='addrecord'>
+                <h3>Add a Record</h3>
+
+                <form onSubmit = {this.handleSubmit}>
+                    <div className="jobform">
+                        <label>Job Title:</label>
+                        <input type="text" onChange={this.handleChange} name="title" value={this.state.title} />
+                        <br />
+
+                        <label>Company:</label>
+                        <input type="text" onChange={this.handleChange} name="company" value={this.state.company} />
+                        <br />
+                        <label>Contact:</label>
+                        <input type="text" onChange={this.handleChange} name="contact" value={this.state.contact} />
+                        <br />
+                        <label>Action: </label>
+                        <input type="text" onChange={this.handleChange} name="action" value={this.state.action} />
+                        <br />
+                        <label>Date:(click for calendar) </label>
+                        <div className='datepicker'>
+                            <DatePicker
+                                selected={this.state.date} onChange={this.handleDate} 
+                            />
+                        </div>
+                       
+                        <label>Note: </label>
+                        <input type="text" onChange={this.handleChange} name="note" value={this.state.note} />
+                        <br />
+                     
+ 
+                       <button className="addjob" type="submit" value="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+export default JobCreate;
---
+++ client/src/components/Resources.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Resources = () => {
+    return (
+        <div className='container' id='resource'>
+            <ul>
+                <h2>List of Resources</h2>
+                <li>Chegg Career Center, https://www.chegg.com/advice/career-center/ </li>
+                <li>Udacity Career Services, https://www.udacity.com/career-services</li>
+                <li>LinkedIn Learning solutions, https://learning.linkedin.com, good resource for video courses</li>
+                <li>Pramp, https://pramp.com, practice live interview with peers</li>
+                <li>The Interview Study Guide for Software Engineers, https://dev.to/seattledataguy/</li>
+                <li>The Balance Careers, How to Prepare for a Job Search- https://thebalancecareers.com</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Resources

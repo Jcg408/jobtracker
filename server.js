2fd7d81a863959dc143d348a50a777464fd29b21
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-12-01 17:38:49 -0500

    add jest to package json and fix server format

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path')
 
 require ('dotenv').config();
 
@@ -23,6 +24,13 @@ const resourcesRouter = require('./routes/resources');
 app.use('/jobs', jobsRouter );
 app.use('/resources', resourcesRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res)=> {
+        res.sendFile(path.join(_dirname, 'client', 'build', 'index.html')); //relative path
+    });
+}
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });

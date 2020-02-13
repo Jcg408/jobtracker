20a875fb097b39390d3bf7beee2b9c6397d5c29a
Jcg408 <jcg321@comcast.net>
2020-02-13 11:13:27 -0500

    adjust clock size and font

--- client/src/components/Today.js
+++ client/src/components/Today.js
@@ -9,8 +9,8 @@ class Today extends Component {
     render() { 
         return ( 
             <div>
-                <h4>  {this.state.today.toLocaleDateString()}</h4>
-                <h4> {this.state.today.toLocaleTimeString()}</h4>
+                <p>  {this.state.today.toLocaleDateString()}</p>
+                <p> {this.state.today.toLocaleTimeString()}</p>
             </div>
          );
     }
